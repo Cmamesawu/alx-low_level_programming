ebb00f2807a4dab9037bd278cd3211193461c40d
Cmamesawu <101551812+Cmamesawu@users.noreply.github.com>
2022-04-20 12:40:14 +0000

    submit 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98-main function that takes a pointer as parameter
+ * Description: updating the value of *n to 98
+ * @n: the pointer
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
